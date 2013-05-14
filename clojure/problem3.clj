cbe12410cc819b186e10c715b07818b6eeeb6604
Austin Bingham <austin.bingham@gmail.com>
2013-05-14 22:46:17 +0200

    Problem 3 in clojure.

--- clojure/problem3.clj
+++ clojure/problem3.clj
@@ -1,13 +1,22 @@
-(:require [clojure.contrib.math :as math])
-
 (defn is_prime [x]
   (cond (= 0 x) false
         (= 1 x) false
         (= 2 x) true
-        (some #(zero? (mod x %)) (take-while (partial > (sqrt x)) primes)) false
+        (some 
+         #(zero? (mod x %))  
+         (take-while (partial >= (Math/sqrt x)) 
+                     (rest (rest (range))))) false
         :else true))
 
 (def primes
   (filter is_prime (range)))
 
-(println (take 10) primes)
+(defn prime_factors 
+  ([x] (prime_factors x primes))
+  ([x cands]
+     (cond (= x 1) '()
+           (zero? (mod x (first cands))) (cons (first cands) (prime_factors (/ x (first cands))))
+           :else (prime_factors x (rest cands)))))
+
+(println (last (prime_factors 600851475143)))
+
