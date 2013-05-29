b8458b3205afccf920fafdc37e1ca95b9eb4627c
Austin Bingham <austin.bingham@emerson.com>
2013-05-29 10:37:27 +0200

    Project euler stuff moved into leiningen project.

--- clojure/project_euler/src/project_euler/util.clj
+++ clojure/project_euler/src/project_euler/util.clj
@@ -4,3 +4,23 @@
   ((fn rfib [a b] 
      (lazy-seq (cons a (rfib b (+ a b)))))
    1 2))
+
+(defn is_prime [x]
+  (cond (= 0 x) false
+        (= 1 x) false
+        (= 2 x) true
+        (some 
+         #(zero? (mod x %))  
+         (take-while (partial >= (Math/sqrt x)) 
+                     (rest (rest (range))))) false
+        :else true))
+
+(def primes
+  (filter is_prime (range)))
+
+(defn prime_factors 
+  ([x] (prime_factors x primes))
+  ([x cands]
+     (cond (= x 1) '()
+           (zero? (mod x (first cands))) (cons (first cands) (prime_factors (/ x (first cands))))
+           :else (prime_factors x (rest cands)))))
--- clojure/project_euler/test/project_euler/core_test.clj
+++ clojure/project_euler/test/project_euler/core_test.clj
@@ -1,7 +1,8 @@
 (ns project-euler.core-test
   (:require [clojure.test :refer :all]
             [project-euler.problem1]
-            [project-euler.problem2]))
+            [project-euler.problem2]
+            [project-euler.problem3]))
 
 (deftest problem1
   (testing "Project Euler problem 1"
@@ -10,3 +11,7 @@
 (deftest problem2
   (testing "Project Euler problem 2"
     (is (= 4613732 (project-euler.problem2/run)))))
+
+(deftest problem3
+  (testing "Project Euler problem 3"
+    (is (= 6857 (project-euler.problem3/run)))))
