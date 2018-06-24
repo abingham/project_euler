521c57bdfbe1007030df7b77f773d90b60022ec2
Austin Bingham <austin.bingham@gmail.com>
2018-06-24 12:32:38 +0200

    Improved ex01.

--- python/euler_1.py
+++ python/euler_1.py
@@ -1,26 +1,20 @@
-sum = 0
-i = 0
-while True:
-    i += 3
-    if i < 1000:
-        sum += i
-    else:
-        break
+from itertools import count, takewhile
 
-i = 0
-while True:
-    i += 5
-    if i < 1000:
-        sum += i
-    else:
-        break
-    i += 5
-    if i < 1000:
-        sum += i
-    else:
-        break
-    i += 5
-    # ignore
 
-print sum
-    
+def multiples(*factors):
+    """Iterable of integers which are multiples of elements of `factors`.
+    """
+    for i in count():
+        if any(i % f == 0 for f in factors):
+            yield i
+
+
+def lt(limit, iterable):
+    """Iterable of elements of `iterable` which are less than `limit`.
+    """
+    return takewhile(lambda i: i < limit, iterable)
+
+
+assert sum(lt(10, multiples(3, 5))) == 23
+
+print(sum(lt(1000, multiples(3, 5))))
