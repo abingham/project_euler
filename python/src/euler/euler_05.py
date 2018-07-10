3ab60946309a2af3ef89d32fede12f4446fca2a2
Austin Bingham <austin.bingham@gmail.com>
2018-07-10 18:15:49 +0200

    Improved exercise 5.

--- python/src/euler/euler_05.py
+++ python/src/euler/euler_05.py
@@ -6,41 +6,8 @@ numbers from 1 to 20?
 
 """
 
-from itertools import count
-
-
-def minimize_divisors(divisors):
-    """Reduce a set of divisors to a minimal set.
-
-    This removes any element in `divisor` which cleanly divides into any other
-    member.
-
-    Note that, for the narrow case of this exercise, this method is overkill.
-    We always deal with all integers from 1 to some value, in which case this
-    function could just return the upper half of that range. But for the more
-    general case of an arbitrary set of divisors, this seems to work.
-
-    Args:
-        divisors: An iterable of integers.
-
-    Returns: An iterable of integers with redundant divisors removed.
-
-    """
-    divisors = sorted(set(divisors))
-    return (d
-            for index, d
-            in enumerate(divisors)
-            if not any(bd % d == 0 for bd in divisors[index + 1:]))
-
-
-def min_divided_by(divisors):
-    divisors = list(minimize_divisors(divisors))
-    divisors.sort(reverse=True)
-    return next(
-        val
-        for val in count(start=divisors[0], step=divisors[0])
-        if all(val % d == 0 for d in divisors))
+from euler.util import least_common_multiple
 
 
 def main():
-    return min_divided_by(range(1, 21))
+    return least_common_multiple(range(1, 21))
--- python/test/test_05.py
+++ python/test/test_05.py
@@ -1,20 +1,8 @@
-from itertools import product
-
-from euler.euler_05 import main, min_divided_by, minimize_divisors
-from hypothesis import given
-import hypothesis.strategies as ST
-
-
-@given(ST.lists(ST.integers(min_value=1)))
-def test_minimize_divisors(divisors):
-    minimized = list(minimize_divisors(divisors))
-    assert not any(x % y == 0
-                   for x, y in product(minimized, minimized)
-                   if x != y)
+from euler.euler_05 import least_common_multiple, main
 
 
 def test_infrastructure():
-    assert min_divided_by(range(1, 11)) == 2520
+    assert least_common_multiple(range(1, 11)) == 2520
 
 
 def test_answer():
