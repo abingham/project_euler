0e51ecf0228dc1eaab536874fd40602f357c227d
Austin Bingham <austin.bingham@gmail.com>
2018-07-10 09:28:50 +0200

    Speeding up Euler 05.

--- python/test/test_05.py
+++ python/test/test_05.py
@@ -1,4 +1,16 @@
-from euler.euler_05 import main, min_divided_by
+from itertools import product
+
+from euler.euler_05 import main, min_divided_by, minimize_divisors
+from hypothesis import given
+import hypothesis.strategies as ST
+
+
+@given(ST.lists(ST.integers(min_value=1)))
+def test_minimize_divisors(divisors):
+    minimized = list(minimize_divisors(divisors))
+    assert not any(x % y == 0
+                   for x, y in product(minimized, minimized)
+                   if x != y)
 
 
 def test_infrastructure():
